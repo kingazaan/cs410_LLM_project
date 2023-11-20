28dec327505befac7d6d6cba35b2d86ae9de28f8
Kaiyao Ke <kaiyaok2@illinois.edu>
2023-11-19 22:57:11 -0600

    added CNN model

---
+++ CNN/main.py
@@ -0,0 +1,101 @@
+#! /usr/bin/env python
+import os
+import argparse
+import datetime
+import torch
+import torchtext.data as data
+import torchtext.datasets as datasets
+import model
+import train
+import mydatasets
+
+parser = argparse.ArgumentParser(description='CNN text classificer')
+# learning
+parser.add_argument('-lr', type=float, default=0.0013, help='initial learning rate [default: 0.0013]')
+parser.add_argument('-epochs', type=int, default=30, help='number of epochs for train [default: 30]')
+parser.add_argument('-batch-size', type=int, default=64, help='batch size for training [default: 64]')
+parser.add_argument('-log-interval',  type=int, default=1,   help='how many steps to wait before logging training status [default: 1]')
+parser.add_argument('-test-interval', type=int, default=50, help='how many steps to wait before testing [default: 50]')
+parser.add_argument('-save-interval', type=int, default=500, help='how many steps to wait before saving [default:500]')
+parser.add_argument('-save-dir', type=str, default='snapshot', help='where to save the snapshot')
+parser.add_argument('-early-stop', type=int, default=1000, help='iteration numbers to stop without performance increasing')
+parser.add_argument('-save-best', type=bool, default=True, help='whether to save when get best performance')
+# data 
+parser.add_argument('-shuffle', action='store_true', default=False, help='shuffle the data every epoch')
+# model
+parser.add_argument('-dropout', type=float, default=0.55, help='the probability for dropout [default: 0.55]')
+parser.add_argument('-max-norm', type=float, default=5.0, help='l2 constraint of parameters [default: 5.0]')
+parser.add_argument('-embed-dim', type=int, default=64, help='number of embedding dimension [default: 64]')
+parser.add_argument('-kernel-num', type=int, default=70, help='number of each kind of kernel [default: 70]')
+parser.add_argument('-kernel-sizes', type=str, default='1,3,5', help='comma-separated kernel size to use for convolution')
+# device
+parser.add_argument('-device', type=int, default=-1, help='device to use for iterate data, -1 mean cpu [default: -1]')
+parser.add_argument('-no-cuda', action='store_true', default=False, help='disable the gpu')
+# option
+parser.add_argument('-snapshot', type=str, default=None, help='filename of model snapshot [default: None]')
+parser.add_argument('-predict', type=str, default=None, help='predict the sentence given')
+parser.add_argument('-test', action='store_true', default=False, help='train or test')
+parser.add_argument('-dataset', type=str, default='financial', help='filename of model snapshot [default: Financial News]')
+args = parser.parse_args()
+# dataset
+
+
+# load my dataset
+def mydataset(text_field, label_field, **kargs):
+    dataset = args.dataset
+    train_data, validation_data, test_data = mydatasets.MyDataset.splits(text_field, label_field, dataset)
+    text_field.build_vocab(train_data, validation_data, test_data)
+    label_field.build_vocab(train_data, validation_data, test_data)
+    train_iter, dev_iter, test_iter = data.Iterator.splits(
+                                (train_data, validation_data, test_data),
+                                batch_sizes=(args.batch_size, len(validation_data), len(test_data)),
+                                **kargs)
+    return train_iter, dev_iter, test_iter
+
+# load data
+print("\nLoading data...")
+text_field = data.Field(lower=True)
+label_field = data.Field(sequential=False)
+train_iter, dev_iter , test_iter = mydataset(text_field, label_field, device=-1, repeat=False)
+
+
+# update args and print
+args.embed_num = len(text_field.vocab)
+args.class_num = len(label_field.vocab) - 1
+args.cuda = (not args.no_cuda) and torch.cuda.is_available(); del args.no_cuda
+args.kernel_sizes = [int(k) for k in args.kernel_sizes.split(',')]
+args.save_dir = os.path.join(args.save_dir, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+
+print("\nParameters:")
+for attr, value in sorted(args.__dict__.items()):
+    print("\t{}={}".format(attr.upper(), value))
+
+
+# model
+cnn = model.CNN_Text(args)
+if args.snapshot is not None:
+    print('\nLoading model from {}...'.format(args.snapshot))
+    cnn.load_state_dict(torch.load(args.snapshot))
+
+if args.cuda:
+    torch.cuda.set_device(args.device)
+    cnn = cnn.cuda()
+        
+
+# train or predict
+if args.predict is not None:
+    label = train.predict(args.predict, cnn, text_field, label_field, args.cuda)
+    print('\n[Text]  {}\n[Label] {}\n'.format(args.predict, label))
+elif args.test:
+    try:
+        train.eval(test_iter, cnn, args)
+    except Exception as e:
+        print("\nSorry. The test dataset doesn't  exist.\n")
+else:
+    print(type(train_iter))
+    try:
+        train.train(train_iter, dev_iter, cnn, args)
+    except KeyboardInterrupt:
+        print('\n' + '-' * 89)
+        print('Exiting from training early')
+
